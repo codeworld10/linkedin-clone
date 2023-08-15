e816a48ebc3af1f1a56c90811aa76cbb18e12f40
Muhammad Usman <manijnb0333@gmail.com>
2023-08-15 14:59:11 +0500

    linkedin-clone

---
+++ components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+ <div className='navbar'>
+ <div className='horizontal-logo'>
+ <div>
+ Logo
+ </div>
+ <div>search</div>
+ </div>
+ <div>
+ menu
+ </div>
+ </div>
+    </>
+  )
+}
+
+export default Navbar
