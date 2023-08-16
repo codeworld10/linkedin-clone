e94a4cce13a2909d5b5644ba4aac37b90f4839d6
Muhammad Usman <manijnb0333@gmail.com>
2023-08-16 14:06:58 +0500

    navbar start

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,46 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { AiFillHome } from 'react-icons/ai'
+import {HiUsers, HiShoppingBag,} from 'react-icons/hi'
+import {IoChatboxEllipsesOutline} from 'react-icons/io5'
+import {BsFillBellFill} from 'react-icons/bs'
 
 const Navbar = () => {
   return (
     <>
- <div className='navbar'>
- <div className='horizontal-logo'>
- <div>
- Logo
- </div>
- <div>search</div>
- </div>
- <div>
- menu
- </div>
- </div>
+      <div className='navbar'>
+        <div className='horizontal-logo'>
+          <div>
+            <Image
+              src="/images/linkedinlogo.png"
+              alt='maniwebdev'
+              width="40"
+              height="40"
+            />
+          </div>
+          <div>
+            <input
+              className='navbar-input'
+              type="text"
+              placeholder="Search..."
+            // value={searchQuery}
+            // onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div>
+          <ul>
+            <li className='navbar-li'>
+              <Link className='navbar-link' href="#home"><div><AiFillHome size={30} /></div> <div>Home</div></Link>
+              <Link className='navbar-link' href="#home"><div><HiUsers size={30}/></div> <div>My network</div></Link>
+              <Link className='navbar-link' href="#home"><div><HiShoppingBag size={30} /></div> <div>Jobs</div></Link>
+              <Link className='navbar-link' href="#home"><div><IoChatboxEllipsesOutline size={30} /></div> <div>Messages</div></Link>
+              <Link className='navbar-link' href="#home"><div><BsFillBellFill size={30} /></div> <div>Notifications</div></Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
