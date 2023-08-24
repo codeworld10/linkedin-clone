a47bea2be24b3d2fe138b903ae49aaa1fa58bbcb
Muhammad Usman <manijnb0333@gmail.com>
2023-08-24 11:40:44 +0500

    check responsive

--- components/Navbar.js
+++ components/Navbar.js
@@ -14,6 +14,7 @@ const Navbar = () => {
         <div className='horizontal-logo'>
           <div className='logo-image'>
             <Image
+            className='logo'
               src="/images/linkedinlogo.png"
               alt='maniwebdev'
               width="35"
