19569738515550d1569844736dd4feb07fc1a73b
Muhammad Usman <manijnb0333@gmail.com>
2023-08-29 11:33:21 +0500

    bottom navigation ready

--- components/BottomNavigation.js
+++ components/BottomNavigation.js
@@ -1,19 +1,28 @@
 // components/BottomNavigation.js
-import Link from 'next/link'; // Import your CSS module
+import Link from 'next/link';
+import { AiFillHome } from 'react-icons/ai';
+import { HiUsers, HiShoppingBag } from 'react-icons/hi';
+import { BsFillBellFill } from 'react-icons/bs';
+import { MdOutlineAddBox } from 'react-icons/md';
 
 const BottomNavigation = () => {
   return (
     <nav className='bottomNav'>
       <Link href="/">
-        <div>Home</div>
+      <div className='bottom-items'><AiFillHome className='bottom-icon' /></div> <div className='bottom-text'>Home</div>
       </Link>
       <Link href="/">
-        <div>Network</div>
+      <div className='bottom-items'><HiUsers className='bottom-icon'/></div> <div className='bottom-text'>My network</div>
       </Link>
       <Link href="/">
-        <div>Jobs</div>
+      <div className='bottom-items'><MdOutlineAddBox className='bottom-icon' /></div> <div className='bottom-text'>Post</div>
       </Link>
-      {/* Add more navigation links */}
+      <Link href="/">
+      <div className='bottom-items'><BsFillBellFill className='bottom-icon' /></div> <div className='bottom-text'>Notifications</div>
+      </Link>
+      <Link href="/">
+      <div className='bottom-items'><HiShoppingBag className='bottom-icon' /></div> <div className='bottom-text'>Jobs</div>
+      </Link>
     </nav>
   );
 };
